675e7b1a8cbfca0ef4ba334349f8e12fbd91b1e7
Umberto <u.petruzzello@studenti.unisa.it>
2018-11-22 00:08:25 +0100

    test

--- Pyroduck/test/pyroduck/level/FileLevelTest.java
+++ Pyroduck/test/pyroduck/level/FileLevelTest.java
@@ -8,7 +8,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import pyroduck.Board;
 import pyroduck.entities.Entity;
+import pyroduck.entities.tile.WallTile;
+import pyroduck.entities.tile.destroyable.BrickTile;
 import pyroduck.exceptions.LoadLevelException;
+import pyroduck.graphics.Screen;
+import pyroduck.graphics.Sprite;
+import pyroduck.input.Keyboard;
 
 /**
  *
@@ -41,20 +46,22 @@ public class FileLevelTest {
     @Test(expected = LoadLevelException.class)
     public void testLoadLevel() throws LoadLevelException{
         System.out.println("loadLevel");
-        String path = "";
-        FileLevel instance = null;
+        String path = "./resoucers/levels/Level1.txt";
+        FileLevel instance = new FileLevel(path);
         instance.loadLevel(path);
     }
 
     /**
      * Test of createEntities method, of class FileLevel.
      */
-    @Test
-    public void testCreateEntities() {
+    @Test(expected = LoadLevelException.class)
+    public void testCreateEntities() throws LoadLevelException{
         System.out.println("createEntities");
-        Board board = null;
-        FileLevel instance = null;
-        Entity[] expResult = null;
+        Board board = new Board(new Keyboard(), new Screen());
+        FileLevel instance = new FileLevel("./resources/level/test.txt");
+        Entity[] expResult = {new WallTile(0, 1, Sprite.wall),
+                              new BrickTile(1, 1, Sprite.brick),
+                              new WallTile(2, 1, Sprite.wall)};
         Entity[] result = instance.createEntities(board);
         assertArrayEquals(expResult, result);
     }
@@ -63,10 +70,10 @@ public class FileLevelTest {
      * Test of getLevel method, of class FileLevel.
      */
     @Test
-    public void testGetLevel() {
+    public void testGetLevel() throws LoadLevelException {
         System.out.println("getLevel");
-        FileLevel instance = null;
-        int expResult = 0;
+        FileLevel instance = new FileLevel("./resources/levels/Level1.txt");
+        int expResult = 1;
         int result = instance.getLevel();
         assertEquals(expResult, result);
     }  
