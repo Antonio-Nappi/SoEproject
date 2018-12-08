68dec156443e34c4db0c57ea5ee1a887b3d0a487
Umberto <u.petruzzello@studenti.unisa.it>
2018-12-08 19:17:52 +0100

    keyboard bug fixed

--- Pyroduck/src/pyroduck/gui/SettingsGame.java
+++ Pyroduck/src/pyroduck/gui/SettingsGame.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pyroduck.gui;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import pyroduck.Board;
 import pyroduck.Game;
 import pyroduck.exceptions.PyroduckException;
 
 /**
  *
- * @author Vincenzo
+ * @author 
  */
 public class SettingsGame extends javax.swing.JFrame {
 
